ba58cb365c17b085b4c2cbe3226dacca8a1a6fc9
MishraSwet <swetaparnamishraofficial@gmail.com>
2024-04-16 23:57:31 +0530

    Changes to signup Component

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,6 +1,8 @@
 import Form from '../components/Form'
 export default function Signup() {
     return <div>
-        <Form label="Name" placeholder='Your Name'/>
+        <Form label="Name" placeholder='Your Name' />
+        <Form label="Email" placeholder='Your Email' />
+        <Form label="Password" placeholder='Password' />
     </div>
 }
