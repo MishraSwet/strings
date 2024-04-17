466abd00f117678a4cbcf854be84b46d9b4e1235
MishraSwet <swetaparnamishraofficial@gmail.com>
2024-04-17 23:55:04 +0530

    Styled FormBtn

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,8 +1,11 @@
+import BtnForm from '../components/BtnForm'
 import Form from '../components/Form'
+
 export default function Signup() {
     return <div>
         <Form label="Name" placeholder='Your Name' />
         <Form label="Email" placeholder='Your Email' />
         <Form label="Password" placeholder='Password' />
+        <BtnForm content="Signup" />
     </div>
 }
