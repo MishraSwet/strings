4b9da3de6c8435acea80e58e44f861bd7fa59eea
MishraSwet <swetaparnamishraofficial@gmail.com>
2024-04-17 23:59:46 +0530

    More Styling Changes

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,7 +2,7 @@ import BtnForm from '../components/BtnForm'
 import Form from '../components/Form'
 
 export default function Signup() {
-    return <div>
+    return <div className="mx-auto w-4/5 px-6 flex justify-center items-center">
         <Form label="Name" placeholder='Your Name' />
         <Form label="Email" placeholder='Your Email' />
         <Form label="Password" placeholder='Password' />
