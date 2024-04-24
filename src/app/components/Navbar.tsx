06d0f74b283824cd48dc93176dadc8e5ed7724f2
MishraSwet <swetaparnamishraofficial@gmail.com>
2024-04-24 23:51:30 +0530

    Navbar Styling

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,15 +2,15 @@ import User from "../user/page"
 import { HomeIcon,AlertIcon,MessageIcon,UserIcon,GroupsIcon,Logout } from "./Icons/NavIcons"
 
 export default function Navbar() {
-    return <div className="w-3/12 flex flex-col justify-around border-2 border-red-200">
-        <div className="flex flex-col">
+    return <div className="w-1/12 flex flex-col border-2 border-red-200 h-screen justify-between">
+        <div className=" flex flex-col mx-auto gap-y-10 p-2 mt-4">
             <HomeIcon />
             <AlertIcon />
             <MessageIcon />
             <UserIcon />
             <GroupsIcon />
         </div>
-        <div>
+        <div className=" m-auto p-2">
             <Logout />
         </div>
     </div>
