deb74cb41d9bb93b47fb95de676ffb7f9401238f
MishraSwet <swetaparnamishraofficial@gmail.com>
2024-04-28 23:31:26 +0500

    Added Imports

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -1,6 +1,7 @@
+import Post from "../components/Post";
 export default function page() {
+
     return <div className="flex flex-col">
-    <Status />
     <Post username="User" title="Post1" tags="friend 1,friend 2" />
     <Post username="User" title="Post1" tags="friend 1,friend 2" />
     <Post username="User" title="Post1" tags="friend 1,friend 2" />
