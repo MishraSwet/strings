2871a77b173d4bedef57e0c3c32faea47db9a62c
MishraSwet <swetaparnamishraofficial@gmail.com>
2024-05-16 23:52:22 +0530

    Added Header Component

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,6 @@
+export default function Header(){
+    return <div className=" w-full p-2 bg-gradient-to-r from-sky-400  to-indigo-700 flex justify-center items-center text-3xl mb-2">
+        Strings
+    </div>
+    
+}
--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import Header from "../components/Header";
 
 export default function signin() {
     return <div>
+        <Header/>
         <div className="grid w-full max-w-sm items-center gap-1.5">
             <Label htmlFor="email">Email</Label>
             <Input className="mb-2" type="email" id="email" placeholder="Email" />
