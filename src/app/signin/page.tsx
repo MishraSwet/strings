ffafa0738c8d17ff7a6c1280320d3c41fa89fda2
MishraSwet <swetaparnamishraofficial@gmail.com>
2024-05-18 23:49:51 +0530

    Cleaned Up Signin Page

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,19 +1,5 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import Header from "../components/Header";
-
-export default function signin() {
+export default function Signin() {
     return <div>
-        <Header/>
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-            <Label htmlFor="email">Email</Label>
-            <Input className="mb-2" type="email" id="email" placeholder="Email" />
-        </div>
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-            <Label htmlFor="password">Password</Label>
-            <Input className="mb-2" type="password" id="password" placeholder="Password" />
-        </div>
-        <Button>signin</Button>
+        Signin Page
     </div>
 }
