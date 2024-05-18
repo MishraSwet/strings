652054d6bc34ef24f77a2afc9d4b31e787b57c1a
MishraSwet <swetaparnamishraofficial@gmail.com>
2024-05-18 23:55:52 +0530

    Updated User Component

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -3,13 +3,14 @@ import Status from "../components/Status";
 import Navbar from "../components/Navbar";
 
 export default function User() {
-    return <Navbar/>
+    return <Navbar />
+    
 }
 
-{/* <div className="flex flex-col">
+<div className="flex flex-col">
     <Status />
     <Post username="User" title="Post1" tags="friend 1,friend 2" />
     <Post username="User" title="Post1" tags="friend 1,friend 2" />
     <Post username="User" title="Post1" tags="friend 1,friend 2" />
     <Post username="User" title="Post1" tags="friend 1,friend 2" />
-</div> */}
+</div> 
