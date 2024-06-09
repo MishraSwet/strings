f98e1c84b5a53bdeb24771c1c69a8bca2a8e8cdb
MishraSwet <swetaparnamishraofficial@gmail.com>
2024-06-09 22:14:37 +0530

    Added Feed and Sidebar Skeletons

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,13 +1,15 @@
 export default function Home() {
-    return <div className="grid grid-cols-3 h-full">
+    return <div className="grid grid-cols-3 h-full justify-items-stretch">
         <Navbar />
+        <Feed />
+        <SideBar />
     </div>
 }
 
 
 import { HomeIcon,AlertIcon,MessageIcon,GroupsIcon,UserIcon,Logout } from "@/components/Icons/NavIcons"
 function Navbar() {
-    return <div className="flex flex-col justify-between h-full w-2/12 p-2">
+    return <div className="flex flex-col justify-between h-full w-2/12 p-2 border-2">
         <div className="flex flex-col justify-between ">
             <HomeIcon/>
             <AlertIcon/>
@@ -19,4 +21,14 @@ function Navbar() {
             <Logout/>
         </div>
     </div>
+}
+
+function Feed() {
+    return <div className="flex flex-col h-full justify-between border-2 w-8/12">
+        Feed
+    </div>
+}
+
+function SideBar() {
+    return <div className="flex flex-col h-full justify-between items-center w-2/12 border-2">Sidebar</div>
 }
