65e13b7e6db84b82145b9eaeb9238210dae4fe37
MishraSwet <swetaparnamishraofficial@gmail.com>
2024-06-23 23:48:27 +0530

    Fixes to typos and structure

--- src/app/alerts/page.tsx
+++ src/app/alerts/page.tsx
@@ -1,8 +1,12 @@
-export default function () {
+export default function Alerts() {
     return <div>
         Alerts Page
         <div>Logo</div>
         <div>User ID</div>
         <div>Text</div>
     </div>
+}
+
+export function AlertComponent() {
+    
 }
