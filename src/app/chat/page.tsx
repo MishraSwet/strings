4c76de44d87bf1ee7b78ef1c99a8e329c4ea5e56
MishraSwet <swetaparnamishraofficial@gmail.com>
2024-06-25 23:58:21 +0530

    Added ChatBar Component

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,11 +1,18 @@
 export default function Chat() {
     return <div>
-        <ChatCard/>
+        <ChatCard />
+        <ChatBar />
     </div>
 }
 
 export function ChatCard() {
     return <div>
-        
+
+    </div>
+}
+
+export function ChatBar() {
+    return <div>
+        ...
     </div>
 }
