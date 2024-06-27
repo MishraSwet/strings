642bc75784340c5685b60d86b0fe6cb574a86fa5
MishraSwet <swetaparnamishraofficial@gmail.com>
2024-06-27 19:21:42 +0530

    Google And Email Auth Configuration

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,30 @@
 import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 
-const handler = NextAuth({ providers:[]});
-export {handler as GET , handler as POST}
+const handler = NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_SECRET as string,
+        }),
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: {
+                    label: "Email",
+                    type: "email",
+                },
+                password: {
+                    label: "Password",
+                    type: "password",
+                },
+            },
+            async authorize(credentials, req) {
 
+            },
+        }),
+    ],
+});
+
+export { handler as GET, handler as POST };
