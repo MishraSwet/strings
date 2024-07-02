bf1df8d33aecb88d7c81f12f0dfac933e8b91acd
MishraSwet <swetaparnamishraofficial@gmail.com>
2024-07-02 23:58:52 +0530

    Skeleton For Alert Socket

--- src/app/api/alerts/route.ts
+++ src/app/api/alerts/route.ts
@@ -2,3 +2,8 @@
 import { WebSocketServer } from 'ws';
 const wss = new WebSocketServer({ newserver: true })
 
+wss.on('connection', (ws) => {
+    ws.on('alert', (alert) => {
+        //Store Alert
+    })
+})
