932c9c21f7d297ebd7022b48abf2a54bb10cbe91
MishraSwet <swetaparnamishraofficial@gmail.com>
2024-07-05 23:28:28 +0530

    Alerts Structure Added

--- src/app/api/alerts/route.ts
+++ src/app/api/alerts/route.ts
@@ -1,9 +1,15 @@
 //Alerts Api Route Go Here
 import { WebSocketServer } from 'ws';
+interface alert{
+    uid: number;
+    message: string;
+}
 const wss = new WebSocketServer({ newserver: true })
 
 wss.on('connection', (ws) => {
-    ws.on('alert', (alert) => {
-        //Store Alert
+    ws.on('alert', (alert:alert) => {
+        const data = JSON.parse(alert);
+        const uid = data.uid;
+        const message = data.message;
     })
 })
