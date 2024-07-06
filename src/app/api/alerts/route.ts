b93d1a825a092a01f3c151ce691d51145b413cbd
MishraSwet <swetaparnamishraofficial@gmail.com>
2024-07-06 23:30:34 +0530

    Added types/ws for types and additional content field to alert

--- src/app/api/alerts/route.ts
+++ src/app/api/alerts/route.ts
@@ -3,13 +3,16 @@ import { WebSocketServer } from 'ws';
 interface alert{
     uid: number;
     action: string;
+    content: string;
 }
 const wss = new WebSocketServer({ newserver: true })
 
-wss.on('connection', (ws) => {
+wss.on('connection', (ws:WebSocketServer) => {
     ws.on('alert', (alert:string) => {
         const data:alert = JSON.parse(alert);
         const uid = data.uid;
         const action = data.action;
+        const content = data.content;
+        const message = uid + "" + action + "Your" + content
     })
 })
