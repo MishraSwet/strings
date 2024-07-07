800b061a03ec3391ce114a857c67e618851d2282
MishraSwet <swetaparnamishraofficial@gmail.com>
2024-07-07 23:59:14 +0530

    Sockets For Chat

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,1 +1,8 @@
-//Route For Chat Socket
+//Alerts Api Route Go Here
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ newserver: true })
+
+
+wss.on('connection', (ws:WebSocketServer) => {
+})
