7db9a0dbe2270ff8623edc278dcec819451d359c
MishraSwet <swetaparnamishraofficial@gmail.com>
2024-07-08 23:59:41 +0530

    Message Interface and Web Sockets

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,8 +1,17 @@
-//Alerts Api Route Go Here
 import { WebSocketServer } from 'ws';
 
-const wss = new WebSocketServer({ newserver: true })
+interface message{
+    content: string,
+    uid:number,
+    target:number
+}
+const wss = new WebSocketServer({ port: 8081 });
 
-
-wss.on('connection', (ws:WebSocketServer) => {
+wss.on('connection', function Connect(ws: WebSocketServer) {
+    ws.on('message', function Message(data: string) {
+        const message:message = JSON.parse(data);
+        const content = message.content;
+        const uid = message.uid;
+        const target = message.target;
+    }
 })
